85de9e06146a955114db9231ec07916c27220509
Genievot <40867747+genievot@users.noreply.github.com>
2019-04-30 04:18:15 +0530

    new commands added
    added help commands

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('guildMemberAdd', member => {
 client.on('message', (message) => {
   if(message.author.bot)
   return;
-  if(message.channel.name === 'p3cbot') {
+  if(message.channel.name === 'accountant-bot') {
     let args = message.content.split(/[\s!]+/)
     if(args[0] === 'info') {
       axios.get('https://api.p3c.io/chart/info').then(res => {
@@ -116,21 +116,140 @@ client.on('message', (message) => {
       }
     }
     if(args[0] === 'help') {
-      message.channel.send({embed: {
-        color: 0x49b86e,
-        title: "Help Command",
-        description: "This can be called to know the list of available comands",
-        fields: [{
-          name: "info",
-          value: "`info` returns the current supply of P3C, the USD Price, the ETC Price, the ETC Market cap, the ETC market cap, and ETC price in USD"
-        },
-        {
-          name: "crop <address>",
-          value: "example: `crop 0xB751eb15542D3fba12065A63f87E0b059c04091C`, Can be called on any crop. Returns the growth in ETC/USD over a 1 day, 7 day, and 30 day period."
-        }]
-      }}).catch(err => {
-        console.log(err);
-      });
+      if(args[1] === 'computer') {
+        message.channel.send({embed: {
+            color: 0x49b86e,
+            title: 'Help computer command',
+            fields:[{
+              name: "Follow this youtube tutorial to setup P3C on your computer",
+              value: 'https://www.youtube.com/watch?v=tBbl_nbp8_k&feature=youtu.be'
+            }]
+          }
+        }).catch(err => {
+          console.log(err);
+        });
+      }
+      if(args[1] === 'saturn') {
+        message.channel.send({embed: {
+            color: 0x49b86e,
+            title: 'Help saturn command',
+            fields:[{
+              name: "Follow this link to install Saturn in your browser",
+              value: 'https://chrome.google.com/webstore/detail/saturn-wallet/nkddgncdjgjfcddamfgcmfnlhccnimig?hl=en'
+            }]
+          }
+        }).catch(err => {
+          console.log(err);
+        });
+      }
+      if(args[1] === 'airdrop') {
+        message.channel.send({embed: {
+            color: 0x49b86e,
+            title: 'Help airdrop command',
+            fields:[{
+              name: "A tutorial explaining how to claim your airdrop. Takes 3 minutes.",
+              value: 'https://forum.saturn.network/t/tutorial-how-to-recieve-a-p3c-io-airdrop-on-your-phone-5-minute-tutorial/4144'
+            }]
+          }
+        }).catch(err => {
+          console.log(err);
+        });
+      }
+      if(args[1] === 'wiki') {
+        message.channel.send({embed: {
+            color: 0x49b86e,
+            title: 'Help wiki command',
+            fields:[{
+              name: "The wiki contains the answers to P3C...",
+              value: 'https://www.youtube.com/watch?v=PuQBHfKVF2I&feature=youtu.be'
+            }]
+          }
+        }).catch(err => {
+          console.log(err);
+        });
+      }
+      if(args[1] === 'phone' || args[1] === 'mobile') {
+        if (args[2] === 'android') {
+          message.channel.send({embed: {
+              color: 0x49b86e,
+              title: 'Help phone android command',
+              fields:[{
+                name: "Install Trust Wallet from the Google Play store",
+                value: 'https://play.google.com/store/apps/details?id=com.wallet.crypto.trustapp'
+              }]
+            }
+          }).catch(err => {
+            console.log(err);
+          });
+        }
+        if (args[2] === 'iphone') {
+          message.channel.send({embed: {
+              color: 0x49b86e,
+              title: 'Help phone iphone command',
+              fields:[{
+                name: "Install Trust Wallet in the App store",
+                value: 'https://itunes.apple.com/us/app/trust-ethereum-wallet/id1288339409'
+              }]
+            }
+          }).catch(err => {
+            console.log(err);
+          });
+        }
+        else {
+          message.channel.send({embed: {
+              color: 0x49b86e,
+              title: 'Help phone/mobile command',
+              fields:[{
+                name: "Follow this youtube tutorial to setup P3C on your phone.Works on Android and iPhone.",
+                value: 'https://www.youtube.com/watch?v=PuQBHfKVF2I&feature=youtu.be'
+              }]
+            }
+          }).catch(err => {
+            console.log(err);
+          });
+        }
+      }
+      else {
+        message.channel.send({embed: {
+          color: 0x49b86e,
+          title: "Help Command",
+          description: "This can be called to know the list of available comands",
+          fields: [{
+            name: "info",
+            value: "`info` returns the current supply of P3C, the USD Price, the ETC Price, the ETC Market cap, the ETC market cap, and ETC price in USD"
+          },
+          {
+            name: "crop <address>",
+            value: "example: `crop 0xB751eb15542D3fba12065A63f87E0b059c04091C`, Can be called on any crop. Returns the growth in ETC/USD over a 1 day, 7 day, and 30 day period."
+          },
+          {
+            name: "help computer",
+            value: "Return Youtube tutorial link to setup P3C on your computer"
+          },
+          {
+            name: "help saturn",
+            value: "Return Link to install Saturn in your browser"
+          },
+          {
+            name: "help phone",
+            value: "Return youtube tutorial to setup P3C on your phone.Works on Android and iPhone."
+          },
+          {
+            name: "help phone android `or` iphone",
+            value: "Return link to Install Trust Wallet app"
+          },
+          {
+            name: "help airdrop",
+            value: "Return a link of a tutorial explaining how to claim your airdrop. Takes 3 minutes."
+          },
+          {
+            name: "help wiki",
+            value: "Return a link of the wiki contains the answers to P3C.."
+          }]
+        }}).catch(err => {
+          console.log(err);
+        });
+      }
     }
   } else {
     return;
